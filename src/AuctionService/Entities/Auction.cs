7cd8bafa256bf3f110f37e12153738f7e0f2b830
Minh Truong <dmtruong@192.168.2.13>
2025-04-29 15:40:31 +0700

    End of section 2

---
+++ src/AuctionService/Entities/Auction.cs
@@ -0,0 +1,26 @@
+using System;
+using AuctionService.Entities;
+
+namespace AuctionService.Entities;
+
+public class Auction
+{
+    public Guid Id { get; set; }
+    public int ReservePrice { get; set; } = 0;
+    public string Seller { get; set; }
+    public string Winner { get; set; }
+    public int? SoldAmount { get; set; }
+    public int? CurrentHighBid { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime AuctionEnd { get; set; } = DateTime.UtcNow;
+    public Status Status { get; set; }
+    public Item Item { get; set; }
+}
+
+
+// AutoMapper.Extensions.Microsoft.DependancyInjection
+// Microsoft.AspNetCore.Authentication.JwtBearer
+// Microsoft.EntityFrameworkCore.Design
+// Npgsql.EntityFrameworkCore.PostgreSQL
+// MassTransit.RabbitMQ
